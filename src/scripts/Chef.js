f9062b4cbb76f0134b88d28363b150ea0e81599d
AlexStLo <alex.st-louis@hotmail.com>
2026-02-24 14:34:31 -0500

    instance de poutines

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -1,10 +1,20 @@
 export default class Chef {
   constructor(element) {
     this.element = element;
+    this.menu = [];
     this.init();
+    //this.container=
   }
 
   init() {
     console.log('Instance de chef');
+    const poutines = this.element.querySelectorAll('.js-poutine');
+    for (let i = 0; i < poutines.length; i++) {
+      const poutine = poutines[i];
+      const instance = new Poutine(poutine);
+      this.menu.push(instance);
+      console.log(this.army);
+      //perso.addEventListener('click', this.togglePerso.bind(this));  n'est plus utile
+    }
   }
 }
